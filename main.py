2cb32674e08a25b77fd98863b23a9429e458c1a6
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-05-24 02:28:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,218 +1,136 @@
-import pygame, threading,os,glob,datetime,timeit
-import time as Time
-from pydub import AudioSegment
+import pygame,glob,datetime,threading,math
+from time import sleep
 import tkinter as tkr
-import multiprocessing
-from itertools import product
-temp=threading.activeCount()
-player=tkr.Tk()
-rate=1
-player.title("Audio player")
-player.geometry("405x240")
-file=tkr.StringVar()
-file_name=tkr.StringVar()
-time_text_str=tkr.StringVar()
-time=tkr.StringVar()
-time.set("0.0")
+from mutagen.mp3 import MP3
 
+player=tkr.Tk()
+player.title("mp3 player")
+player.geometry("505x240")
+
+file_name=tkr.StringVar()
 time_text_end=tkr.StringVar()
+time_text_str=tkr.StringVar()
+time_temp=0
+time=tkr.StringVar()
+time.set("0")
 time_text_end.set("/ 0:00:00")
 time_text_str.set("0:00:00")
-
-wf=""
-next_bool=False
-pause=False
-index_time=0
-song_list=[]
-song_list_not_org=[]
+file=""
 elements=5
-global p
 progress=""
-#default_index=0
 playlist=tkr.Listbox(player,highlightcolor="blue",selectmode=tkr.SINGLE)
+next_file=""
+pause=False
+stop_threads=True
 songs=[]
-
-lst=[]
-def works(song):
-    global song_list,song_list_not_org,pos
-    if os.path.getsize(os.getcwd() + "\\" + song)//1000000<40:
-        try:
-            # print(song)
-            print("Fail" + song, os.path.getsize(os.getcwd() + "\\" + song))
-            #wf = AudioSegment.from_file_using_temporary_files(song)
-            song_list.append(song)
-            char_list = [song[j] for j in range(len(song)) if ord(song[j]) in range(65536)]
-            song = ''
-            for j in char_list:
-                song = song + j
-            song_list_not_org.append(str(song_list_not_org.__len__()+1)+"."+song)
-            #playlist.delete(0, tkr.END)
-            #for song in song_list_not_org:
-            #    playlist.insert(pos, song)
-            #    pos += 1
-            ## print("Pass-------" + song, os.path.getsize(os.getcwd() + "\\" + song))
-        except:
-            print("Fail" + song, os.path.getsize(os.getcwd() + "\\" + song))
-            pass
-
 for song in glob.glob("*.mp3"):
-    #song_list.append(song)
-    works(song)
-    #t = threading.Thread(target=works, args=(song,))
-    #t.start()
+    songs.append(song)
+song_list_not_org=[]
 pos=0
-for song in song_list_not_org:
-    playlist.insert(pos, song)
-    pos += 1
-stream=""
-stop_threads=False
-def stream_builder(file):
-    import pyaudio
-    import wave
-    global p
-    filename = file
+def fix():
+ global pos,song_list_not_org
+ for song in songs:
 
-    # Set chunk size of 1024 samples per data frame
-    chunk = 1024
+  char_list = [song[j] for j in range(len(song)) if ord(song[j]) in range(65536)]
+  song = ''
+  for j in char_list:
+   song = song + j
+  song_list_not_org.append(str(song_list_not_org.__len__() + 1) + "." + song)
+  playlist.insert(pos,song_list_not_org[-1])
+  pos += 1
+fix()
+def count_time():
+ global stop_threads,progress,pause
+ print(stop_threads)
+ while stop_threads and str(time_text_end.get())[1:]!=str(time_text_str.get()):
+  while pause==True:
+   pass
+  sleep(0.1)
+  if progress.get()<float(time.get())-2 or progress.get()>float(time.get())+2:
+   time.set(int(progress.get()))
+   pygame.mixer.music.stop()
+   pygame.mixer.music.load(songs[song_list_not_org.index(file)])
+   pygame.mixer.music.play(0,int(float(time.get())))
+  else:
+   time.set(str(float(time.get())+0.1))
+   progress.set(float(time.get()))
+  time_text_str.set(str(datetime.timedelta(seconds=round(float(time.get())))))
+  #print(time.get())
+ time.set("0")
+ time_text_str.set("0")
+ stop_threads=True
 
-    # Open the sound file
-    # wf = wave.open(filename, 'rb')
-    # Create an interface to PortAudio
-    p = pyaudio.PyAudio()
+def play():
+ global pause,progress,stop_threads,file
+ if pause==True and file==playlist.get(tkr.ACTIVE):
+  pygame.mixer.music.unpause()
+  pause=False
+  return 0
+ if file!="" and file!=playlist.get(tkr.ACTIVE):
+  pause=False
+  stop_threads=False
+  time.set("0")
+ pygame.init()
+ pygame.mixer.init()
+ temp_file_name=songs[song_list_not_org.index(playlist.get(tkr.ACTIVE))]
+ pygame.mixer.music.load(temp_file_name)
+ file=playlist.get(tkr.ACTIVE)
+ pygame.mixer.music.play(0,int(float(time.get())))
+ audio = MP3(temp_file_name)
+ print(audio.info.length)
+ cutter=int(math.log10(int(song_list_not_org.index(playlist.get(tkr.ACTIVE))))+2)
+ file_name.set(str(playlist.get(tkr.ACTIVE))[cutter:])
+ time_text_end.set(str("/"+str(datetime.timedelta(seconds=round(audio.info.length,0)))))
+ progress = tkr.Scale(player, from_=0, to_=round(audio.info.length,0), orient=tkr.HORIZONTAL, resolution=0.1)
+ progress.grid(row=2, columnspan=elements, sticky=tkr.EW)
+ x = threading.Thread(target=count_time, args=())
+ x.start()
 
-    # Open a .Stream object to write the WAV file to
-    # 'output = True' indicates that the sound will be played rather than recorded
-    stream = p.open(format=p.get_format_from_width(wf.sample_width),
-                    channels=wf.channels,
-                    rate=wf.frame_rate,
-                    output=True)
-
-    return stream
-def Play():
-    global file,index_time,time,progress,next_bool
-    global stream,wf,stop_threads,time_text_end
-
-    #if next_bool:
-    #    next_bool=False
-    #    index_time=0
-    #    file.set(playlist.selection_get())
-    #    wf = AudioSegment.from_file(str(file.get()))
-    #    stream=stream_builder(str(file.get()))
-    #    temp=wf.frame_count()/wf.frame_rate
-#
-    #    progress = tkr.Scale(player, from_=0.0, to_=round(temp,1), orient=tkr.HORIZONTAL, resolution=0.1)
-    #    progress.grid(row=2,columnspan=elements,sticky=tkr.EW)
-    if  file.get()!=playlist.get(tkr.ACTIVE) or next_bool:
-        index_time=0
-        file.set(playlist.get(tkr.ACTIVE))
-        file_name.set(song_list[song_list_not_org.index(file.get())])
-        print(file.get())
-        #file_w="Øfdream - Thelema [ Bass boosted,Slowed ]-y5C.mp3"
-
-        wf = AudioSegment.from_file_using_temporary_files(song_list[song_list_not_org.index(file.get())])
-        #wf = AudioSegment.from_file_using_temporary_files(file_w)
-        stream=stream_builder(song_list[song_list_not_org.index(file.get())])###
-
-        temp=wf.frame_count()/wf.frame_rate
-        time_str_format = str(datetime.timedelta(seconds=wf.frame_count()//wf.frame_rate))
-        if temp<3600:
-            time_str_format=time_str_format[2:]
-        time_text_end.set("/"+time_str_format)
-
-
-        progress = tkr.Scale(player, from_=0.0, to_=wf.frame_count()//wf.frame_rate, orient=tkr.HORIZONTAL, resolution=0.1)
-        progress.grid(row=2,columnspan=elements,sticky=tkr.EW)
-
-    # Read data in chunks
-
-    # Play the sound by writing the audio data to the stream
-    i=index_time*4410+1
-    data = wf.get_frame(i-1)
-    print(wf.frame_rate, wf.frame_count()/wf.frame_rate)
-
-    while data != ''  and stop_threads==False:
-        while i % 4410 != 0 and stop_threads==False:
-            stream.write(data)
-            data = wf.get_frame(i)
-            i += 1
-        while pause and stop_threads==False:
-            pass
-        print(progress.get())
-        if progress.get() < round(i / 44100, 2) - 1 or progress.get() > round(i / 44100, 2) + 1:
-            i = int(progress.get() * 4410) * 10
-            data = wf.get_frame(i)
-        else:
-            index_time = i // 4410
-
-        ## time
-        time_str_format=str(datetime.timedelta(seconds=i // 44100))
-        if wf.frame_count()/wf.frame_rate<3600:
-            time_str_format=time_str_format[2:]
-        time_text_str.set(time_str_format)
-        # print(round(i / 44100,2),wf.__len__()//4410,progress.get())
-
-        progress.set(round(i / 44100, 2))
-        stream.write(data)
-        data = wf.get_frame(i + 1)
-        i += 1
-    x = threading.Thread(target=Play, args=())
-    stop_threads = False
-    # Close and terminate the stream
-global x
-def Stop():
-    global pause,x
-    pause=True
-
-def test():
-    global pause,x,stop_threads
-    pause=False
-    try:
-        if x.is_alive() == False:
-            x.start()
-        else:
-            if file.get()!=song_list[song_list_not_org.index(playlist.get(tkr.ACTIVE))]:
-                stop_threads = True
-                test()
-    except:
-
-        x = threading.Thread(target=Play, args=())
-        x.start()
+def pause():
+ global pause
+ pygame.mixer.music.pause()
+ pause=True
+ return 0
 
 def Next():
-    global file,stop_threads,next_bool,x,pause
-    pause=False
-    next_song=""
-    lst=[]
-    for song in song_list_not_org:
-        lst.append(song)
-        print(song)
+ global songsm,file
+ stop_threads = False
+ if songs[-1] == file_name.get():
+  next_file=(songs[0])
+ else:
+  next_file =(songs[songs.index(file_name.get()) + 1])
+
+ playlist.activate(songs.index(next_file))
+ file = playlist.get(tkr.ACTIVE)
+ pygame.mixer.music.stop()
+ time.set("0")
+ time_text_str.set("0")
+ play()
+ return 0
 
 
-    if lst[-1]==file.get():
-        file.set(lst[0])
-    else:
-        file.set(lst[lst.index(file.get())+1])
-    playlist.activate(lst.index(file.get()))
-    playlist.selection_clear(0,lst.__len__())
-    playlist.select_set(lst.index(file.get()))
-    print(tkr.ACTIVE)
-    next_bool=True
-    if x.is_alive():
-        stop_threads = True
-        Time.sleep(1)
-        x = threading.Thread(target=Play, args=())
-        x.start()
+def test():
+ global pause, x, stop_threads
+ pause = False
+ try:
+  if x.is_alive() == False:
+   x.start()
+  else:
+   if file_name.get() !=playlist.get(tkr.ACTIVE):
+    stop_threads = False
+ except:
+  x = threading.Thread(target=play, args=())
+  x.start()
 
-Butten_play=tkr.Button(player,text="PLAY",command=test)
-Butten_stop=tkr.Button(player,text="PAUSE",command=Stop)
+
+Butten_play=tkr.Button(player,text="PLAY",command=play)
+Butten_stop=tkr.Button(player,text="PAUSE",command=pause)
 Butten_next=tkr.Button(player,text="NEXT",command=Next)
 current_song=tkr.Label(player,textvariable=file_name)
 time_text_str.set("0.0")
 time_text=tkr.Label(player,textvariable=time_text_str)
 time_end=tkr.Label(player,textvariable=time_text_end)
-
-
-progress=tkr.Scale(player,from_=0.0,to_=time.get(),orient=tkr.HORIZONTAL,resolution=0.1)
+progress=tkr.Scale(player,from_=0,to_=time.get(),orient=tkr.HORIZONTAL,resolution=0.1)
 
 Butten_play.grid(row=3,sticky="w")
 Butten_stop.grid(row=3,column=1, sticky="w")
@@ -222,8 +140,8 @@ time_end.grid(row=3,column=4,sticky="w")
 
 progress.grid(row=2,columnspan=elements,sticky=tkr.EW)
 current_song.grid(row=1,columnspan=elements,sticky=tkr.N)
-
 playlist.grid(row=0,columnspan=elements,sticky="NSEW")
+
 player.columnconfigure(0, weight=1)
 player.columnconfigure(1, weight=1)
 player.columnconfigure(2, weight=1)
@@ -234,5 +152,5 @@ player.rowconfigure(1, weight=1)
 player.rowconfigure(2, weight=1)
 player.rowconfigure(3, weight=1)
 player.mainloop()
-stream.close()
-p.terminate()
+
+player.mainloop()
